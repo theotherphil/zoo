d36ab94e4ebdeae11b11832d56e07fc86b98fa55
Phil Ellison <phil.j.ellison@gmail.com>
2018-11-12 21:25:59 +0000

    Grr

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,26 @@ fn main() {
     let db = vec![
         // sibling(X,Y) :- child(X,Z), child(Y,Z).
         Assertion::new(
-            Atom::new("sibling".into(), vec![var("X"), var("Y")]),
+            Atom::new("sibling", vec![var("X"), var("Y")]),
             Clause::new(vec![
-                Atom::new("child".into(), vec![var("X"), var("Z")]),
-                Atom::new("child".into(), vec![var("Y"), var("Z")])
+                Atom::new("child", vec![var("X"), var("Z")]),
+                Atom::new("child", vec![var("Y"), var("Z")])
             ])
         ),
         // child("luke", "vader")
         Assertion::new(
-            Atom::new("child".into(), vec![var("luke"), var("vader")]),
+            Atom::new("child", vec![Term::Const("luke".into()), Term::Const("vader".into())]),
             Clause::empty()
         ),
         // child("leia", "vader")
         Assertion::new(
-            Atom::new("child".into(), vec![var("leia"), var("vader")]),
+            Atom::new("child", vec![Term::Const("leia".into()), Term::Const("vader".into())]),
             Clause::empty()
         )
     ];
     // ?- sibling(X, Y).
-    let clause = Clause::new(vec![Atom::new("sibling".into(), vec![var("X"), var("Y")])]);
-    solve_top_level(clause, db).unwrap();
+    let clause = Clause::new(vec![Atom::new("sibling", vec![var("X"), var("Y")])]);
+    solve_top_level(clause, db);
 }
 
 // String starting with a lower case letter
@@ -66,8 +66,8 @@ struct Atom {
 }
 
 impl Atom {
-    fn new(sym: Constant, terms: Vec<Term>) -> Atom {
-        Atom { sym, terms }
+    fn new(sym: impl Into<Constant>, terms: Vec<Term>) -> Atom {
+        Atom { sym: sym.into(), terms }
     }
 }
 
@@ -104,42 +104,54 @@ impl Assertion {
 }
 
 // The current values of variables
-type Environment = Vec<(Variable, Term)>;
-
-// The current program
-type Database = Vec<Assertion>;
-
 #[derive(Debug, Clone, PartialEq, Eq)]
-enum TopLevelCommand {
-    Assert(Assertion),
-    Goal(Clause)
+struct Environment {
+    mapping: Vec<(Variable, Term)>
 }
 
-// Returns the value of x in env, or Var(x) if the variable
-// is not contained in the environment
-fn lookup(env: Environment, x: Variable) -> Term {
-    env.iter()
-        .find(|vt| vt.0 == x)
-        .map_or(Term::Var(x), |vt| vt.1.clone())
+impl Environment {
+    fn new(mapping: Vec<(Variable, Term)>) -> Environment {
+        Environment { mapping }
+    }
+
+    // Returns the value of x in env, or Var(x) if the variable
+    // is not contained in the environment
+    fn lookup(&self, x: Variable) -> Term {
+        self.mapping
+            .iter()
+            .find(|vt| vt.0 == x)
+            .map_or(Term::Var(x), |vt| vt.1.clone())
+    }
+
+    fn is_empty(&self) -> bool {
+        self.mapping.is_empty()
+    }
+    
+    fn empty() -> Self {
+        Environment { mapping: vec![] }
+    }
 }
 
 #[test]
 fn test_lookup_found() {
-    let env = vec![
+    let env = Environment::new(vec![
         (Variable::new("X", 0), Term::Const("value".into()))
-    ];
+    ]);
     let x = Variable::new("X", 0);
-    assert_eq!(lookup(env, x), Term::Const("value".into()));
+    assert_eq!(env.lookup(x), Term::Const("value".into()));
 }
 
 #[test]
 fn test_lookup_not_found() {
-    let env = vec![
+    let env = Environment::new(vec![
         (Variable::new("X", 0), Term::Const("value".into()))
-    ];
+    ]);
     let y = Variable::new("Y", 0);
-    assert_eq!(lookup(env, y.clone()), Term::Var(y.clone()));
+    assert_eq!(env.lookup(y.clone()), Term::Var(y.clone()));
 }
+
+// The current program
+type Database = Vec<Assertion>;
 
 // subst_term(sub, t) subsitutes variables in term t for values
 // specified by sub. It substitutes repeatedly until the terms
@@ -151,7 +163,7 @@ fn subst_term(env: &Environment, t: &Term) -> Term {
 
     match t {
         Term::Var(x) => {
-            let e = lookup(env.clone(), x.clone());
+            let e = env.lookup(x.clone());
             if e == Term::Var(x.clone()) { 
                 e
             } else {
@@ -164,8 +176,41 @@ fn subst_term(env: &Environment, t: &Term) -> Term {
             ts.iter()
                 .map(|l| subst_term(env, l))
                 .collect()
-            )
+        )
     }
+}
+
+#[test]
+fn test_subst_term_const() {
+    let env = Environment::new(vec![
+        (Variable::new("X", 0), Term::Const("value".into()))
+    ]);
+    assert_eq!(
+        subst_term(&env, &Term::Const("const".into())),
+        Term::Const("const".into())
+    );
+}
+
+#[test]
+fn test_subst_term_variable() {
+    let env = Environment::new(vec![
+        (Variable::new("X", 0), Term::Const("value".into()))
+    ]);
+    assert_eq!(
+        subst_term(&env, &Term::Var(Variable::new("X", 0))),
+        Term::Const("value".into())
+    );
+}
+
+#[test]
+fn test_subst_term_app() {
+    let env = Environment::new(vec![
+        (Variable::new("X", 0), Term::Const("value".into()))
+    ]);
+    assert_eq!(
+        subst_term(&env, &Term::App("foo".into(), vec![Term::Const("x".into()), Term::Var(Variable::new("X", 0))])),
+        Term::App("foo".into(), vec![Term::Const("x".into()), Term::Const("value".into())])
+    );
 }
 
 // Converts a term t to its string representation
@@ -184,11 +229,13 @@ fn string_of_term(t: &Term) -> String {
 // Converts an environment to it its string representation,
 // only keeping variables at level 0
 fn string_of_env(env: &Environment) -> String {
-    let env = env.clone().into_iter().filter(|(Variable { name: _, depth: n }, _)| *n == 0).collect::<Vec<_>>();
-    if env.len() == 0 {
+    let env = Environment::new(env.clone().mapping.into_iter().filter(|(Variable { name: _, depth: n }, _)| *n == 0).collect::<Vec<_>>());
+    if env.is_empty() {
         String::from("Yes")
     } else {
-        env.iter()
+        env
+            .mapping
+            .iter()
             .rev()
             .map(|(Variable { name: x, depth: _ }, t)| format!(
                 "{} = {}",
@@ -210,7 +257,7 @@ fn occurs(x: &Variable, t: &Term) -> bool {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 struct NoUnify;
 
 // Unifies terms t1 and t2 in the current environment.
@@ -225,24 +272,43 @@ fn unify_terms(env: &Environment, t1: &Term, t2: &Term) -> Result<Environment, N
     let s1 = subst_term(env, t1);
     let s2 = subst_term(env, t2);
 
-    if s1 == *t1 && s2 == *t2 {
+    if s1 == *t1 && s2 == *t2 && s1 == s2 {
+        println!("  terms equal after substitution");
         return Ok(env.clone());
     }
 
     match (&s1, &s2) {
         (Term::Var(y), t) | (t, Term::Var(y)) => {
-            if occurs(y, t) { Err(NoUnify) } else {
+            println!("  (var, t) or (t, var)");
+            if occurs(y, t) {
+                println!("    no unify");
+                Err(NoUnify)
+            } else {
                 let mut r = env.clone();
-                r.insert(0, (y.clone(), t.clone()));
+                r.mapping.insert(0, (y.clone(), t.clone()));
+                println!("    new_env: {:?}", r);
                 Ok(r)
             }
         },
         (Term::Const(_), _) => Err(NoUnify),
         (Term::App(c1, ts1), Term::App(c2, ts2)) if c1 == c2 => {
+            println!("  apps with matching syms");
             unify_lists(env, ts1.clone(), ts2.clone())
         },
         (Term::App(_, _), _) => Err(NoUnify)
     }
+}
+
+#[test]
+fn unify_terms_match_after_subst() {
+    let env = Environment::new(vec![
+        (Variable::new("X", 0), Term::Const("x".into())),
+        (Variable::new("Y", 0), Term::Const("y".into()))
+    ]);
+    let t1 = Term::App("foo".into(), vec![Term::Var(Variable::new("X", 0)), Term::Const("y".into())]);
+    let t2 = Term::App("foo".into(), vec![Term::Const("x".into()), Term::Var(Variable::new("Y", 0))]);
+    let new_env = unify_terms(&env, &t1, &t2);
+    assert_eq!(Ok(env), new_env);
 }
 
 // Unifies two lists in current environment and returns a new environment on success.
@@ -274,7 +340,7 @@ fn unify_atoms(env: &Environment, a1: &Atom, a2: &Atom) -> Result<Environment, N
 // for another solution for a clause. Final item is the search depth
 type Choice = (Database, Environment, Clause, i32);
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 struct NoSolution;
 
 // Renumbers all variable instances occurring in a term so that they have level n
@@ -296,19 +362,19 @@ fn renumber_atom(n: i32, a: &Atom) -> Atom {
 // for other solutions, as described by the list of choice points ch, to to abort the
 // current proof search
 fn display_solution(ch: &[Choice], env: &Environment) -> Result<(), NoSolution> {
-    println!("ENV IS: {:?}", env);
+    println!("\n\n\n\n\n  DISPLAY_SOLUTION (env: {:?})", env);
 
     let env_str = string_of_env(env);
     if env_str == "Yes" {
-        println!("Yes");
+        println!("\n\n\n\n\n  Yes");
         return Ok(());
     }
     if ch.len() == 0 {
-        println!("{}", env_str);
+        println!("\n\n\n\n\n    Env: {}", env_str);
         return Ok(());
     }
-    // TODO: support searching for more solutions
-    let answer = "n";
+    // TODO: support stopping search for more solutions
+    let answer = "y";
     match answer {
         "y" => continue_search(ch),
         _ => Err(NoSolution)
@@ -336,23 +402,33 @@ fn reduce_atom(a: Atom, env: Environment, db: Database, n: i32) -> Option<(Datab
     println!("  n: {}", n);
 
     if db.len() == 0 {
-            None
+        None
     } else {
         let Assertion { head: b, body: lst } = &db[0];
         let rest = db.clone().into_iter().skip(1).collect::<Vec<_>>();
         let maybe_new_env = unify_atoms(&env, &a, &renumber_atom(n, b));
         match maybe_new_env {
-            Ok(new_env) => Some((rest, new_env, Clause::new(lst.conjuncts.iter().map(|l| renumber_atom(n, l)).collect::<Vec<_>>()))),
-            Err(_) => reduce_atom(a, env, rest, n)
+            Ok(new_env) => {
+                let new_clause = Clause::new(lst.conjuncts.iter().map(|l| renumber_atom(n, l)).collect::<Vec<_>>());
+                println!("  **** new_env: {:?}", new_env);
+                println!("  **** rest: {:?}", rest);
+                println!("  **** new_clause: {:?}", new_clause);
+                return Some((rest, new_env, new_clause));
+            },
+            Err(_) => {
+                println!("  failed to unify atoms. rest: {:?}", rest);
+                reduce_atom(a, env, rest, n)
+            }
         }
     }
 }
 
 // Searches for a proof of clause c.
 fn solve(ch: &[Choice], db: Database, env: Environment, c: Clause, n: i32) -> Result<(), NoSolution> {
-    println!("C LENGTH = {}. n = {}", c.conjuncts.len(), n);
+    println!("SOLVE (c.length = {}, n = {})", c.conjuncts.len(), n);
 
     if c.is_empty() {
+        println!("  clause is empty");
         // All atoms are solved, we found a solution
         return display_solution(ch, &env);
     }
@@ -372,6 +448,7 @@ fn solve(ch: &[Choice], db: Database, env: Environment, c: Clause, n: i32) -> Re
             let mut new_ch = ch.iter().map(|x| x.clone()).collect::<Vec<_>>();
             new_ch.insert(0, (new_db, env.clone(), c, n));
             d.conjuncts.extend(cp);
+            println!("  d: {:?}", d);
             solve(&new_ch, db.clone(), new_env, d, n + 1)
         }
     }
@@ -379,5 +456,8 @@ fn solve(ch: &[Choice], db: Database, env: Environment, c: Clause, n: i32) -> Re
 
 // Searches for the proof of clause c using the provided global database
 fn solve_top_level(c: Clause, db: Database) -> Result<(), NoSolution> {
-    solve(&vec![], db, vec![], c, 1)
+    println!("SOLVING");
+    println!("  c: {:?}", c);
+    println!("  db: {:?}", db);
+    solve(&vec![], db, Environment::empty(), c, 1)
 }
